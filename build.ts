847fd4877796a618db95c48e05a858e2119bda05
Sai Ashirwad <saiashirwadr@gmail.com>
2024-11-24 06:27:28 +0530

    init

--- build.ts
+++ build.ts
@@ -1,20 +1,20 @@
-import type { BuildConfig } from 'bun'
-import dts from 'bun-plugin-dts'
+import type { BuildConfig } from "bun";
+import dts from "bun-plugin-dts";
 
 const defaultBuildConfig: BuildConfig = {
-  entrypoints: ['./src/index.ts'],
-  outdir: './dist'
-}
+	entrypoints: ["./src/index.ts"],
+	outdir: "./dist",
+};
 
 await Promise.all([
-  Bun.build({
-    ...defaultBuildConfig,
-    plugins: [dts()],
-    format: 'esm',
-    naming: "[dir]/[name].js",
-  }),
-  Bun.build({
-    ...defaultBuildConfig,
-    format: 'esm',
-  })
-])
+	Bun.build({
+		...defaultBuildConfig,
+		plugins: [dts()],
+		format: "esm",
+		naming: "[dir]/[name].js",
+	}),
+	Bun.build({
+		...defaultBuildConfig,
+		format: "esm",
+	}),
+]);
