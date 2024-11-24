79fc9ded2027d4166a8ff0656524b82ebefcccb6
Sai Ashirwad <saiashirwadr@gmail.com>
2024-11-24 18:19:18 +0530

    eh

--- examples/main.ts
+++ examples/main.ts
@@ -1,25 +1,85 @@
-import { computed, createSignal, lazyComputed } from '../src'
+import { computed, createEffect, createSignal } from '../src'
 
-const count = createSignal(0)
+const inputEl = document.getElementById('input') as HTMLInputElement
+// const outputEl = document.getElementById('output') as HTMLDivElement
+// const duplicatedOutputEl = document.getElementById(
+// 	'duplicated-output',
+// ) as HTMLDivElement
+// const duplicatedOutput2El = document.getElementById(
+// 	'duplicated-output-2',
+// ) as HTMLDivElement
 
-const doubled = computed(() => count.get() * 2)
-const doubledLazy = lazyComputed(() => count.get() * 2)
+// function bindText<T>(
+// 	element: HTMLDivElement | HTMLSpanElement | HTMLParagraphElement,
+// 	signal: Signal<T> | Computed<T>,
+// 	decode: (value: T) => string,
+// ) {
+// 	createEffect(() => {
+// 		element.textContent = decode(signal.get())
+// 	})
+// }
 
-// DOM elements
-const countEl = document.getElementById('count')!
-const doubledEl = document.getElementById('doubled')!
-const doubledLazyEl = document.getElementById('doubled-lazy')!
-const incrementBtn = document.getElementById('increment')!
+// function onEvent(
+// 	element: Element,
+// 	event: keyof HTMLElementEventMap,
+// 	handler: (e: Event) => void,
+// ) {
+// 	element.addEventListener(event, handler)
+// 	return () => {
+// 		element.removeEventListener(event, handler)
+// 	}
+// }
 
-function updateDOM() {
-	countEl.textContent = count.get().toString()
-	doubledEl.textContent = doubled.get().toString()
-	doubledLazyEl.textContent = doubledLazy.get().toString()
-}
+// function bindEvent<T>(
+// 	element: Element,
+// 	signal: Signal<T>,
+// 	event: keyof HTMLElementEventMap,
+// 	decode: (value: T) => string,
+// 	encode: (value: string) => T,
+// ) {
+// 	onEvent(element, event, (e) => {
+// 		signal.set(encode((e.target as HTMLInputElement).value))
+// 	})
+// 	createEffect(() => {
+// 		if ('value' in element) {
+// 			element.value = decode(signal.get())
+// 		} else if ('textContent' in element) {
+// 			element.textContent = decode(signal.get())
+// 		}
+// 	})
+// }
 
-updateDOM()
-
-incrementBtn.addEventListener('click', () => {
-	count.set(count.get() + 1)
-	updateDOM()
+const inputSignal = createSignal('')
+inputEl.addEventListener('input', (e) => {
+	inputSignal.set((e.target as HTMLInputElement).value)
 })
+
+// bindEvent(
+// 	inputEl,
+// 	inputSignal,
+// 	'input',
+// 	(value) => value,
+// 	(value) => value,
+// )
+
+createEffect(() => {
+	console.log('inputSignal', inputSignal.get())
+})
+
+const duplicatedInput = computed(() => {
+	const input = inputSignal.get()
+	return `${input}${input}`
+})
+
+const length = computed(() => duplicatedInput.get().length)
+
+createEffect(() => {
+	const currentLength = length.get()
+	const currentDuplicated = duplicatedInput.get()
+	console.log({ length: currentLength })
+	console.log({ duplicatedInput: currentDuplicated })
+})
+
+// bindText(outputEl, duplicatedInput, (value) => value)
+// bindText(duplicatedOutputEl, duplicatedInput, (value) => value)
+// bindText(duplicatedOutput2El, duplicatedInput, (value) => value)
